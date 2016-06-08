607d7dfffbb2c6c8fd1b6135ac1fe5706435f2a6
PRODIGI <rahul_meghlan@apple.com>
2016-06-08 18:30:45 +0530

    added abs urls from python site

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,18 +1,21 @@
 'use strict';
 angular.module("webCrawlerApp").controller("MainController", ['$scope', 'MainService', function($scope, mainService){
-  $scope.fetchUrls = function(){
-    mainService.getLinks($scope.initUrl).then(function(res){
+
+
+  $scope.validateAndCrawl = function(){
+    mainService.getLinks($scope.crawlUrl).then(function(res){
       console.log("Res : ", res);
+      $scope.absUrls = res.data.success.absUrls;
     })
   };
 
   function initScope(){
-    $scope.initUrl = "http://www.python.org";
+    $scope.crawlUrl = "http://www.python.org";
+    $scope.isValidUrl = "/(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/";
   }
 
   function init(){
     initScope();
-    $scope.fetchUrls();
   }
 
   init();
