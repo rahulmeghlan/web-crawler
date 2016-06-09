be65f080333adab7fc0b770e2a6053551c159fb8
PRODIGI <rahul_meghlan@apple.com>
2016-06-09 23:54:33 +0530

    fixed implementation for upto 3 urls

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,23 +1,62 @@
 'use strict';
 angular.module("webCrawlerApp").controller("MainController", ['$scope', 'MainService', function($scope, mainService){
-
-
+  var index = 0,
+  subIndex = 0;
+  /**
+  * Used to validate the entered url and crawl for links in it.
+  */
   $scope.validateAndCrawl = function(){
+    //$scope.newSearch = true;
     mainService.getLinks($scope.crawlUrl).then(function(res){
-      console.log("Res : ", res);
-      $scope.absUrls = res.data.success.absUrls;
-    })
-  };
+    if(index < 3){
+      $scope.resultsTables.push(index);
+      $scope.absUrls[index] = res.data.success.absUrls
+      $scope.crawlUrl = $scope.absUrls[subIndex][index];
+      $scope.crawledUrls.push($scope.crawlUrl);
+      $scope.validateAndCrawl();
+      //$scope.newSearch = false;
+      index++;
+    }
+  })
+};
+/**
+* @description : This function is used to search a custom url
+*/
+$scope.resetSearch = function(){
+  $scope.crawlUrl = "";
+  //$scope.newSearch = false;
+  $scope.absUrls = {};
+  $scope.resultsTables = [];
+};
 
-  function initScope(){
-    $scope.crawlUrl = "http://www.python.org";
-    $scope.isValidUrl = "/(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/";
-  }
+/**
+* @description : This function is used to bind events on this controller
+*/
+function bindEvents(){
+}
+/**
+* @description : This function is used to init scope for this controller
+*/
+function initScope(){
+  $scope.newSearch = false;
+  $scope.absUrls = {};
+  $scope.resultsTables = [];
+  $scope.crawlUrl = "http://www.python.org";
+  $scope.crawledUrls = [$scope.crawlUrl];
+  $scope.isValidUrl = "/(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/";
+  $scope.validateAndCrawl();
+}
+/**
+* @description : This function is used to initialize all required inner functions of the controller
+*/
+function init(){
+  initScope();
+  bindEvents();
+}
 
-  function init(){
-    initScope();
-  }
-
-  init();
+/**
+* call internal function init to initialize the controller functions
+*/
+init();
 
 }]);
