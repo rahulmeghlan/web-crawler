100d9060cf15a368d0c925770f3181623a3649ac
PRODIGI <rahul_meghlan@apple.com>
2016-06-10 11:08:37 +0530

    fixed multiple issues in UI validation

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,62 +1,77 @@
 'use strict';
 angular.module("webCrawlerApp").controller("MainController", ['$scope', 'MainService', function($scope, mainService){
   var index = 0,
-  subIndex = 0;
+  subIndex = 0,
+  validUrlPattern = new RegExp(/(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/);
   /**
   * Used to validate the entered url and crawl for links in it.
   */
   $scope.validateAndCrawl = function(){
     //$scope.newSearch = true;
-    mainService.getLinks($scope.crawlUrl).then(function(res){
-    if(index < 3){
-      $scope.resultsTables.push(index);
-      $scope.absUrls[index] = res.data.success.absUrls
-      $scope.crawlUrl = $scope.absUrls[subIndex][index];
-      $scope.crawledUrls.push($scope.crawlUrl);
-      $scope.validateAndCrawl();
-      //$scope.newSearch = false;
-      index++;
+    if(validUrlPattern.test($scope.crawlUrl)){
+      mainService.getLinks($scope.crawlUrl).then(function(res){
+        if(res.data.success){
+          if(index < 3){
+            $scope.resultsTables.push(index);
+            $scope.absUrls[index] = res.data.success.absUrls
+            $scope.crawlUrl = $scope.absUrls[subIndex][index];
+            $scope.crawledUrls.push($scope.crawlUrl);
+            $scope.validateAndCrawl();
+            index++;
+          }else{
+            $scope.crawlUrl = "";
+            $scope.crawledUrls.splice($scope.crawledUrls.length-1, 1);
+            $scope.searchInProgress = false;
+          }
+        }else{
+          alert("Sorry, no links found.")
+        }
+      })
+    }else{
+      alert("Enter a valid url !");
     }
-  })
-};
-/**
-* @description : This function is used to search a custom url
-*/
-$scope.resetSearch = function(){
-  $scope.crawlUrl = "";
-  //$scope.newSearch = false;
-  $scope.absUrls = {};
-  $scope.resultsTables = [];
-};
+  };
+  /**
+  * @description : This function is used to search a custom url
+  */
+  $scope.resetSearch = function(){
+    //$scope.newSearch = false;
+    index = 0;
+    subIndex = 0;
+    $scope.absUrls = {};
+    $scope.resultsTables = [];
+    $scope.crawledUrls = [$scope.crawlUrl];
+    $scope.validateAndCrawl();
+  };
 
-/**
-* @description : This function is used to bind events on this controller
-*/
-function bindEvents(){
-}
-/**
-* @description : This function is used to init scope for this controller
-*/
-function initScope(){
-  $scope.newSearch = false;
-  $scope.absUrls = {};
-  $scope.resultsTables = [];
-  $scope.crawlUrl = "http://www.python.org";
-  $scope.crawledUrls = [$scope.crawlUrl];
-  $scope.isValidUrl = "/(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/";
-  $scope.validateAndCrawl();
-}
-/**
-* @description : This function is used to initialize all required inner functions of the controller
-*/
-function init(){
-  initScope();
-  bindEvents();
-}
+  /**
+  * @description : This function is used to bind events on this controller
+  */
+  function bindEvents(){
+  }
+  /**
+  * @description : This function is used to init scope for this controller
+  */
+  function initScope(){
+    $scope.newSearch = false;
+    $scope.absUrls = {};
+    $scope.resultsTables = [];
+    $scope.searchInProgress = true;
+    $scope.crawlUrl = "http://www.python.org";
+    $scope.crawledUrls = [$scope.crawlUrl];
+    $scope.validateAndCrawl();
+  }
+  /**
+  * @description : This function is used to initialize all required inner functions of the controller
+  */
+  function init(){
+    initScope();
+    bindEvents();
+  }
 
-/**
-* call internal function init to initialize the controller functions
-*/
-init();
+  /**
+  * call internal function init to initialize the controller functions
+  */
+  init();
 
 }]);
